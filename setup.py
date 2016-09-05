a32b1c25b5477789f58ec1c09fa524156df2a5a3
Tomasz bla Fortuna <bla@thera.be>
2016-09-05 16:25:26 +0200

    Optimized mixing function using Cython.
    Will complicate deployments a bit.
    + added key_file function to not leave the "secret" passphrase visible in process list

--- setup.py
+++ setup.py
@@ -2,16 +2,27 @@
 # coding: utf-8
 
 from setuptools import setup
+from setuptools.extension import Extension
+
+
+try:
+    from Cython.Build import cythonize
+    USE_CYTHON=True
+except ImportError:
+    USE_CYTHON=False
+    cythonize = lambda x: x
+ext = '.pyx' if USE_CYTHON else '.c'
+
 
 setup(
     name = "barySSH",
-    version = "v0.2",
+    version = "v0.3",
     author = "Tomasz Fortuna",
     author_email = "bla@thera.be",
     description = ("Proxy which uses simple time-based XOR scheme "
                    "to make heuristic detection of underlying "
                    "transmission impossible."),
-    install_requires = ['twisted>=14.0.0'],
+    install_requires = ['twisted>=14.0.0', 'cython>=0.21.1'],
     license = "MIT",
     keywords = "ssh xor masking proxy tunnel",
     url = "https://github.com/blaa/barySSH",
@@ -21,4 +32,10 @@ setup(
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
     ],
+    ext_modules = cythonize(
+        Extension(
+            "barylib/barylib",
+            ["barylib/barylib.pyx"], # " + ext],
+        )
+    )
 )
